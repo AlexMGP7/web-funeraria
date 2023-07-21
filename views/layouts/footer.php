8062b8f39f3db6cd3feb58bf0382434dc037facc
Alexander Gonzalez <agonzalezpastena7a7t@gmail.com>
2023-07-21 15:34:12 -0400

    footer fixeado

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,5 +1,5 @@
-<footer class="bg-dark text-white text-center py-4 fixed-bottom">
+<footer class="bg-dark text-white text-center py-3">
     <div class="container">
-        &copy; <?php echo date("Y"); ?> Polizas Funerarias. Todos los derechos reservados.
+      &copy; <?php echo date("Y"); ?> Polizas Funerarias. Todos los derechos reservados.
     </div>
-</footer>
+  </footer>
