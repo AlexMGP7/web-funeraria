35b2bd594c7ce458a5fb166c88a1cfd77bd0f8e3
Alexander Gonzalez <agonzalezpastena7a7t@gmail.com>
2023-07-21 15:35:11 -0400

    poniendole main a layout

--- views/layouts/layout.php
+++ views/layouts/layout.php
@@ -1,24 +1,3 @@
-<?php
-session_start();
-
-if (isset($_SESSION['mensaje']) && isset($_SESSION['mensaje_tipo'])) {
-    $mensaje = $_SESSION['mensaje'];
-    $mensaje_tipo = $_SESSION['mensaje_tipo'];
-    unset($_SESSION['mensaje']);
-    unset($_SESSION['mensaje_tipo']);
-?>
-
-    <div class="alert alert-<?php echo $mensaje_tipo; ?>">
-        <?php echo $mensaje; ?>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-
-<?php
-}
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 
@@ -36,20 +15,45 @@ if (isset($_SESSION['mensaje']) && isset($_SESSION['mensaje_tipo'])) {
 </head>
 
 <body>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
     <header>
         <?php require_once('../../views/layouts/header.php'); ?>
         <?php require_once('../../views/layouts/submenu.php'); ?>
     </header>
 
-    <section>
-        <div class="container">
-            <?php
-            // carga el archivo routing.php para direccionar a la página .php que se incrustará entre la header y el footer
-            require_once('../../views/routing.php');
-            ?>
+    <?php
+    session_start();
+
+    if (isset($_SESSION['mensaje']) && isset($_SESSION['mensaje_tipo'])) {
+        $mensaje = $_SESSION['mensaje'];
+        $mensaje_tipo = $_SESSION['mensaje_tipo'];
+        unset($_SESSION['mensaje']);
+        unset($_SESSION['mensaje_tipo']);
+    ?>
+
+        <div class="alert alert-<?php echo $mensaje_tipo; ?>">
+            <?php echo $mensaje; ?>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
-    </section>
+
+    <?php
+    }
+    ?>
+
+    <main>
+
+        <section>
+            <div class="container">
+                <?php
+                // carga el archivo routing.php para direccionar a la página .php que se incrustará entre la header y el footer
+                require_once('../../views/routing.php');
+                ?>
+            </div>
+        </section>
+
+    </main>
 
     <footer>
         <?php require('../../views/layouts/footer.php'); ?>
@@ -58,13 +62,14 @@ if (isset($_SESSION['mensaje']) && isset($_SESSION['mensaje_tipo'])) {
     <!-- Scripts JavaScript -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
     <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
     <script>
         $(document).ready(function() {
             $('#dtBasicExample').DataTable({
-                "lengthMenu": [10, 15, 20],
+                "lengthMenu": [5, 10, 15, 30],
                 "language": {
                     "sEmptyTable": "No se encontraron resultados",
                     "sInfo": "Mostrando _START_ - _END_ de _TOTAL_ resultados",
