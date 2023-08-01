f483e7431b9c1e81377baa586dd31adb7c05eef9
Alexander Gonzalez <agonzalezpastena7a7t@gmail.com>
2023-07-31 23:10:12 -0400

    rj

--- views/ResponsableJ/insert_responsableJ.php
+++ views/ResponsableJ/insert_responsableJ.php
@@ -1,65 +1,67 @@
  <?php
- // Verificar si se han enviado los datos del formulario a través de POST
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Obtener los datos del formulario
-    $rif = $_POST['rif'];
-    $correo = $_POST['correo'];
-    $telefono = $_POST['telefono'];
-    $razon_social = $_POST['razon_social'];
-     require_once('../../controllers/responsableJ_controller.php');
-    $controller = new ResponsableJuridicoController();
-     // Insertar el responsable jurídico y obtener el resultado
-    $result_responsable_juridico = $controller->IngresarResponsableJ2($rif, $correo, $telefono, $razon_social);
-     // Verificar si el insert fue exitoso
-    if ($result_responsable_juridico) {
-        $_SESSION['mensaje'] = "El responsable jurídico se ha registrado correctamente.";
-        $_SESSION['mensaje_tipo'] = "success";
-    } else {
-        $_SESSION['mensaje'] = "Error al registrar el responsable jurídico. Por favor, intenta nuevamente.";
-        $_SESSION['mensaje_tipo'] = "warning";
+    // Verificar si se han enviado los datos del formulario a través de POST
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Obtener los datos del formulario
+        $rif = $_POST['rif'];
+        $correo = $_POST['correo'];
+        $telefono = $_POST['telefono'];
+        $razon_social = $_POST['razon_social'];
+
+        //echo $rif,$correo,$telefono,$razon_social;
+
+        require_once('../../controllers/responsableJ_controller.php');
+        $controller = new ResponsableJuridicoController();
+        // Insertar el responsable jurídico y obtener el resultado
+        $result_responsable_juridico = $controller->IngresarResponsableJ2($rif, $correo, $telefono, $razon_social);
+        // Verificar si el insert fue exitoso
+        if ($result_responsable_juridico) {
+            $_SESSION['mensaje'] = "El responsable jurídico se ha registrado correctamente.";
+            $_SESSION['mensaje_tipo'] = "success";
+        } else {
+            $_SESSION['mensaje'] = "Error al registrar el responsable jurídico. Por favor, intenta nuevamente.";
+            $_SESSION['mensaje_tipo'] = "warning";
+        }
+        // Redirigir a la página de listado de responsables jurídicos después de intentar insertar.
+        // echo '<script>window.location.href = "?controller=ResponsableJ&action=ListarResponsablesJ";</script>';
+        // exit();
     }
-     // Redirigir a la página de listado de responsables jurídicos después de intentar insertar.
-    // echo '<script>window.location.href = "?controller=ResponsableJ&action=ListarResponsablesJ";</script>';
-    // exit();
-}
 
-require_once('../../controllers/personaJuridica_controller.php');
-$persona_juridica_controller = new PersonaJuridicaController();
-$result_persona_juridica = $persona_juridica_controller->ListarPersonaJ1();
+    require_once('../../controllers/personaJuridica_controller.php');
+    $persona_juridica_controller = new PersonaJuridicaController();
+    $result_persona_juridica = $persona_juridica_controller->ListarPersonaJ1();
 
-
-?>
+    ?>
  <div class="container-i mt-5">
-    <div class="page-content">
-        <form action="?controller=ResponsableJ&action=IngresarResponsableJ" method="POST">
-            <div class="custom-form-background p-4">
-                <h4>Ingreso de Responsables Jurídicos</h4>
-                <div class="form-group">
-                    <label for="rif"><b>RIF:</b></label>
-                    <select class="form-control" name="rif" id="rif" required>
-                        <?php
-                        while ($row_persona_juridica = mysqli_fetch_array($result_persona_juridica)) {
-                            $rif_persona_juridica = $row_persona_juridica['rif'];
-                            $nombre_persona_juridica = $row_persona_juridica['nombre'];
-                            echo "<option value='$rif_persona_juridica'>$rif_persona_juridica - $nombre_persona_juridica</option>";
-                        }
-                        ?>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="correo"><b>Correo:</b></label>
-                    <input class="form-control" type="email" name="correo" id="correo" maxlength="100" required placeholder="Ingrese aquí el correo del responsable jurídico" />
-                </div>
-                <div class="form-group">
-                    <label for="telefono"><b>Teléfono:</b></label>
-                    <input class="form-control" type="text" name="telefono" id="telefono" maxlength="20" required placeholder="Ingrese aquí el teléfono del responsable jurídico" />
-                </div>
-                <div class="form-group">
-                    <label for="razon_social"><b>Razón Social:</b></label>
-                    <input class="form-control" type="text" name="razon_social" id="razon_social" maxlength="100" required placeholder="Ingrese aquí la razón social del responsable jurídico" />
-                </div>
-                <button class="btn btn-success" type="submit">Ingresar</button>
-            </div>
-        </form>
-    </div>
-</div>
+     <div class="page-content">
+         <form action="?controller=ResponsableJ&action=IngresarResponsableJ" method="POST">
+             <div class="custom-form-background p-4">
+                 <h4>Ingreso de Responsables Jurídicos</h4>
+                 <div class="form-group">
+                     <label for="rif"><b>RIF:</b></label>
+                     <select class="form-control" name="rif" id="rif" required>
+                         <?php
+                            while ($row_persona_juridica = mysqli_fetch_array($result_persona_juridica)) {
+                                $rif_persona_juridica = $row_persona_juridica['rif'];
+                                $nombre_persona_juridica = $row_persona_juridica['nombre'];
+                                echo "<option value='$rif_persona_juridica'>$rif_persona_juridica - $nombre_persona_juridica</option>";
+                            }
+                            ?>
+                     </select>
+                 </div>
+                 <div class="form-group">
+                     <label for="correo"><b>Correo:</b></label>
+                     <input class="form-control" type="email" name="correo" id="correo" maxlength="100" required placeholder="Ingrese aquí el correo del responsable jurídico" />
+                 </div>
+                 <div class="form-group">
+                     <label for="telefono"><b>Teléfono:</b></label>
+                     <input class="form-control" type="text" name="telefono" id="telefono" maxlength="20" required placeholder="Ingrese aquí el teléfono del responsable jurídico" />
+                 </div>
+                 <div class="form-group">
+                     <label for="razon_social"><b>Razón Social:</b></label>
+                     <input class="form-control" type="text" name="razon_social" id="razon_social" maxlength="100" required placeholder="Ingrese aquí la razón social del responsable jurídico" />
+                 </div>
+                 <button class="btn btn-success" type="submit">Ingresar</button>
+             </div>
+         </form>
+     </div>
+ </div>
